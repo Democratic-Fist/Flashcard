2b4276a2bb73914c0459ea2802cdf9f84a4b22e5
Super Earth Armed Forces - SEAF <jose.lino.chaves@escola.pr.gov.br>
2025-06-27 10:01:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'História',
+    'Qual foi a principal causa da queda do Império Romano do Ocidente?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'História',
+    'Como o Tratado de Versalhes, assinado após a Primeira Guerra Mundial, influenciou os eventos que levaram à Segunda Guerra Mundial?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'História',
+    'Quem foi responsável pela unificação da Alemanha no século XIX e quais foram os principais desafios desse processo?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'História',
+    'Quais foram as principais contribuições do Renascimento para o desenvolvimento da arte, ciência e filosofia na Europa?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'História'
+    'Quais as principais diferenças entre o sistema feudal medieval e o sistema de produção capitalista que emergiu no final da Idade Média?'
+    
+)
