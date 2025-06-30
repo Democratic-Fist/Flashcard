fb9e6164555b6a7364a59e89e5bbbcfc6a2a731b
Super Earth Armed Forces - SEAF <jose.lino.chaves@escola.pr.gov.br>
2025-06-30 08:20:02 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -23,7 +23,120 @@ criaCartao(
 )
 
 criaCartao(
-    'História'
-    'Quais as principais diferenças entre o sistema feudal medieval e o sistema de produção capitalista que emergiu no final da Idade Média?'
-    
+    'História',
+    'Quais as principais diferenças entre o sistema feudal medieval e o sistema de produção capitalista que emergiu no final da Idade Média?',
+
+)
+
+criaCartao(
+    'Hitória',
+    'Como as viagens de Cristóvão Colombo e outros exploradores do século XV afetaram o encontro entre o Velho e o Novo Mundo?',
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula para calcular a área de um triângulo?',
+    'A = (base*altura)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como você resolve uma equação quadrática do tipo ax2+bx+c=0?',
+
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a diferença entre uma progressão aritmética (PA) e uma progressão geométrica (PG)? Dê um exemplo de cada.',
+
+)
+
+criaCartao(
+    'Matemática',
+    'Como calcular o valor de π e por que ele é importante para os cálculos envolvendo círculos?',
+
+)
+
+criaCartao(
+    'Matemática',
+    'O que é uma função exponencial?',
+
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula para o cálculo da soma dos termos de uma progressão aritmética finita?',
+
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a principal diferença entre o clima temperado e o clima tropical, e em que regiões do mundo eles predominam?',
+
+)
+
+criaCartao(
+    'Geografia',
+    'Como o desmatamento na Amazônia afeta o clima global e a biodiversidade?',
+
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são as principais características dos biomas de desertos e como eles se adaptam às condições extremas de temperatura e umidade?',
+
+)
+
+criaCartao(
+    'Geografia',
+    'O que são placas tectônicas e como o movimento delas está relacionado a fenômenos como terremotos e erupções vulcânicas?',
+
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são as principais diferenças entre o continente europeu e o continente africano em termos de população, economia e relevo?',
+
+)
+
+criaCartao(
+    'Geografia',
+    'Como o conceito de urbanização influencia a formação de megacidades, e quais são as consequências disso para o meio ambiente e a qualidade de vida?',
+
+)
+
+criaCartao(
+    'Esportes',
+    'Qual foi o primeiro país a ganhar a Copa do Mundo de Futebol e em que ano isso aconteceu?',
+
+)
+
+criaCartao(
+    'Esportes',
+    'Quais são as principais diferenças entre o basquete e o vôlei em termos de regras e objetivos do jogo?',
+
+)
+
+criaCartao(
+    'esportes',
+    'O que caracteriza uma corrida de Fórmula 1 e quais são os principais fatores que determinam o desempenho de um piloto?',
+
+)
+
+criaCartao(
+    'esportes',
+    'Como o ciclismo de pista é diferente do ciclismo de estrada em termos de técnicas e competições?',
+
+)
+
+criaCartao(
+    'Esportes',
+    'Quem é o maior vencedor de Grand Slams na história do tênis, e quais são os maiores torneios da modalidade?',
+
+)
+
+criaCartao(
+    'Esportes',
+    'O que é o "triplo-duplo" no basquete e quais são os desafios para um jogador alcançar essa estatística em uma partida?',
+
 )
