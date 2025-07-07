c8073c6cd5cfe95a161187586695d9f2bd0325c5
Super Earth Armed Forces - SEAF <jose.lino.chaves@escola.pr.gov.br>
2025-07-07 08:20:02 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,36 +1,37 @@
 criaCartao(
     'História',
     'Qual foi a principal causa da queda do Império Romano do Ocidente?',
-    'O Python é uma linguagem de programação'
+    'As principais razões incluem as invasões bárbaras, a instabilidade política, a crise econômica, a corrupção e o enfraquecimento do poder militar.'
 )
 
 criaCartao(
     'História',
     'Como o Tratado de Versalhes, assinado após a Primeira Guerra Mundial, influenciou os eventos que levaram à Segunda Guerra Mundial?',
-    'A capital da França é Paris'
+    'ao impor pesadas reparações financeiras, perda de território, restrições militares e a responsabilização exclusiva da Alemanha pela guerra, gerou um profundo ressentimento e humilhação no povo alemão.'
 )
 
 criaCartao(
     'História',
-    'Quem foi responsável pela unificação da Alemanha no século XIX e quais foram os principais desafios desse processo?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Quem foi responsável pela unificação da Alemanha no século XIX?',
+    'A unificação da Alemanha no século XIX foi liderada principalmente por Otto von Bismarck, o primeiro-ministro da Prússia.'
 )
 
 criaCartao(
     'História',
     'Quais foram as principais contribuições do Renascimento para o desenvolvimento da arte, ciência e filosofia na Europa?',
-    'Oi em ingles é HI (RAI)'
+    ' A valorização do ser humano (antropocentrismo), o resgate da cultura greco-romana e o desenvolvimento do racionalismo foram características centrais desse movimento, que impactaram profundamente o desenvolvimento europeu. '
 )
 
 criaCartao(
     'História',
-    'Quais as principais diferenças entre o sistema feudal medieval e o sistema de produção capitalista que emergiu no final da Idade Média?',
-
+    'Quando surgiu o sistema capitalisa, principal modo de produção no mundo atual?',
+    'O capitalismo surgiu gradualmente a partir do século XIII, com o declínio do sistema feudal e o fortalecimento do comércio e da produção artesanal.'
 )
 
 criaCartao(
     'Hitória',
     'Como as viagens de Cristóvão Colombo e outros exploradores do século XV afetaram o encontro entre o Velho e o Novo Mundo?',
+    'As viagens de Cristóvão Colombo e outros exploradores marcaram o início de um período de intercâmbio cultural, exploração, colonização e, infelizmente, também de violência e sofrimento para as populações indígenas. '
 )
 
 criaCartao(
@@ -47,80 +48,80 @@ criaCartao(
 
 criaCartao(
     'Matemática',
-    'Qual é a diferença entre uma progressão aritmética (PA) e uma progressão geométrica (PG)? Dê um exemplo de cada.',
-
+    'Qual é a diferença entre uma progressão aritmética (PA) e uma progressão geométrica (PG)?',
+    'Em uma PA, a razão é somada ao termo anterior para obter o próximo, enquanto em uma PG, a razão é multiplicada pelo termo anterior.'
 )
 
 criaCartao(
     'Matemática',
     'Como calcular o valor de π e por que ele é importante para os cálculos envolvendo círculos?',
-
+    'É um número irracional obtido através da divisão do coprimento de uma circunferência pelo seu diâmetro.'
 )
 
 criaCartao(
     'Matemática',
     'O que é uma função exponencial?',
-
+    ' é aquela em que a variável independente (geralmente "x") aparece como expoente de uma base constante (geralmente "a"), representada por f(x) = a^x, onde a > 0 e a ≠ 1.'
 )
 
 criaCartao(
     'Matemática',
     'Qual é a fórmula para o cálculo da soma dos termos de uma progressão aritmética finita?',
-
+    'A fórmula para calcular a soma dos termos de uma progressão aritmética (PA) finita é: Sn = (a1 + an) * n / 2, onde:  Sn: é a soma dos n termos da PA. a1: é o primeiro termo da PA. an: é o último termo da PA. n: é o número de termos da PA.'
 )
 
 criaCartao(
     'Geografia',
     'Qual é a principal diferença entre o clima temperado e o clima tropical, e em que regiões do mundo eles predominam?',
-
+    'O clima tropical, localizado entre os trópicos de Câncer e Capricórnio, possui temperaturas mais elevadas e maiores períodos de chuvas. Já o clima temperado, situado entre os trópicos e os círculos polares, possui temperaturas mais equilibradas e chuvas regulares.'
 )
 
 criaCartao(
     'Geografia',
     'Como o desmatamento na Amazônia afeta o clima global e a biodiversidade?',
-
+    'A perda da floresta contribui para o aquecimento global, altera os padrões de chuva e afeta a biodiversidade local, levando à extinção de espécies e desequilíbrio ecológico. '
 )
 
 criaCartao(
     'Geografia',
-    'Quais são as principais características dos biomas de desertos e como eles se adaptam às condições extremas de temperatura e umidade?',
-
+    'Quais são as principais características dos biomas de desertos?',
+    'Os desertos são caracterizados pela baixa umidade e escassez de chuvas, o que leva a temperaturas extremas e variações significativas entre o dia e a noite'
 )
 
 criaCartao(
     'Geografia',
-    'O que são placas tectônicas e como o movimento delas está relacionado a fenômenos como terremotos e erupções vulcânicas?',
-
+    'O que são placas tectônicas?',
+    'As placas tectônicas são grandes fragmentos rochosos que compõem a camada externa da Terra (litosfera), flutuando sobre o manto terrestre.'
 )
 
 criaCartao(
     'Geografia',
-    'Quais são as principais diferenças entre o continente europeu e o continente africano em termos de população, economia e relevo?',
-
+    'Quais são as principais características do continente europeu em termos de população, economia e relevo?',
+    'A população é a segunda menor entre os continentes, com cerca de 747 milhões de habitantes.  A economia europeia é diversificada, com destaque para a indústria, agricultura e serviços. O relevo é marcado por planícies, planaltos e cadeias montanhosas.'
 )
 
 criaCartao(
     'Geografia',
-    'Como o conceito de urbanização influencia a formação de megacidades, e quais são as consequências disso para o meio ambiente e a qualidade de vida?',
-
+    'Como o conceito de urbanização influencia a formação de megacidades?',
+    'O rápido crescimento urbano, impulsionado pela industrialização e migração do campo para a cidade, leva ao desenvolvimento de grandes aglomerações urbanas com mais de 10 milhões de habitantes, as megacidades.'
 )
 
 criaCartao(
     'Esportes',
     'Qual foi o primeiro país a ganhar a Copa do Mundo de Futebol e em que ano isso aconteceu?',
-
+    'O primeiro país a ganhar a Copa do Mundo de Futebol foi o Uruguai, na edição de 1930. A competição foi realizada no próprio Uruguai, e a seleção uruguaia venceu a Argentina na final por 4 a 2.'
 )
 
 criaCartao(
     'Esportes',
     'Quais são as principais diferenças entre o basquete e o vôlei em termos de regras e objetivos do jogo?',
-
+    'O basquete visa marcar pontos ao arremessar a bola na cesta adversária, enquanto o voleibol busca fazer com que a bola toque o chão da quadra adversária passando por cima da rede.'
 )
 
 criaCartao(
     'esportes',
-    'O que caracteriza uma corrida de Fórmula 1 e quais são os principais fatores que determinam o desempenho de um piloto?',
-
+    'Quais são os principais fatores que determinam o desempenho de um piloto de Fórmula 1?',
+    'Os principais fatores que determinam o desempenho de um piloto incluem sua habilidade de pilotagem, capacidade de tomar decisões rápidas e precisas, resistência física e mental, e a capacidade de colaborar com a equipe para otimizar o carro e a estratégia.'
 )
 
 criaCartao(
